d042186c01f302338eb57fae3a61c9463705edaf
ivanlalic <ivanlalic@gmail.com>
2023-03-21 17:43:40 -0300

    server working for url

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const fetch = require("node-fetch-commonjs");
-const FormData = require('form-data');
+const FormData = require("form-data");
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
+const fs = require("fs");
 
 let subtitles = {};
 
@@ -17,80 +18,86 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
+app.post("/post", upload.single("file"), async (req, res) => {
+  if (req.body.url) {
+    // Handle request with URL parameter
+    const data = {
+      apiKey: "E4gLjTpp2x",
+      model: "tiny",
+      language: "auto-detect",
+      shouldTranslate: false,
+      downloadLink: req.body.url,
+    };
 
+    const response = await fetch("https://freesubtitles.ai/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
 
-  app.post('/post', upload.single('file'), async (req, res) => {
-
-    const { url } = req.body;
-    if (url) {
-      const data = {
-        apiKey: "E4gLjTpp2x",
-        model: "tiny",
-        language: "auto-detect",
-        shouldTranslate: false,
-        downloadLink: url,
-      };
-      const response = await fetch("https://freesubtitles.ai/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (response.ok) {
-        subtitles = await response.json();
-        console.log(subtitles);
-        if (subtitles.transcribeDataEndpoint) {
-          res.send(subtitles);
-        }
-      } else {
-        console.error("Failed to get subtitles:", response.statusText);
+    if (response.ok) {
+      const subtitles = await response.json();
+      console.log(subtitles);
+      if (subtitles.transcribeDataEndpoint) {
+        res.send(subtitles);
       }
     } else {
-      const file = req.file;
-      const formData = new FormData();
-      formData.append("file", file.buffer, file.originalname);
-      const response = await fetch("https://freesubtitles.ai/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        body: formData,
-      });
-  
-      if (response.ok) {
-        subtitles = await response.json();
-        console.log(subtitles);
-        if (subtitles.transcribeDataEndpoint) {
-          res.send(subtitles);
-        }
-      } else {
-        console.error("Failed to get subtitles:", response.statusText);
-      }
+      console.error("Failed to get subtitles:", response.statusText);
     }
-  });
+  } else if (req.file && req.file.path && req.file.originalname) {
+    // Handle request with file upload
+    const stream = fs.createReadStream(req.file.path);
 
-  app.get('/get', async (req, res) => {
-    console.log(subtitles.transcribeDataEndpoint)
-    if (subtitles.transcribeDataEndpoint) {
-      try {
-        const response = await fetch(subtitles.transcribeDataEndpoint);
-        const data = await response.json();
-        console.log(data);
-        res.send(data);
-      } catch (error) {
-        console.error("Error:", error);
+    const formData = new FormData();
+    formData.append("file", stream, req.file.originalname);
+    formData.append("apiKey", "E4gLjTpp2x");
+    formData.append("model", "tiny");
+    formData.append("language", "auto-detect");
+    formData.append("shouldTranslate", "false");
+
+    const response = await fetch("https://freesubtitles.ai/api", {
+      method: "POST",
+      headers: formData.getHeaders(),
+      body: formData,
+    });
+
+    if (response.ok) {
+      const subtitles = await response.json();
+      console.log(subtitles);
+      if (subtitles.transcribeDataEndpoint) {
+        res.send(subtitles);
       }
     } else {
-      console.error("Please enter a transcribeDataEndpoint");
+      console.error("Failed to get subtitles:", response.statusText);
     }
+  } else {
+    console.error("File data is missing or incomplete:", req.file);
+  }
+});
+
+app.get("/get", async (req, res) => {
+  console.log(subtitles.transcribeDataEndpoint);
+  if (subtitles.transcribeDataEndpoint) {
+    try {
+      const response = await fetch(subtitles.transcribeDataEndpoint);
+      const data = await response.json();
+      console.log(data);
+      res.send(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  } else {
+    console.error("Please enter a transcribeDataEndpoint");
+  }
 });
 
 app.listen(port, () => {
